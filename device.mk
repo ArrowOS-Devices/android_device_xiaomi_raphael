46428b1f499ecd7c3fe35699704497f2d3803909
Park Ju Hyung <qkrwngud825@gmail.com>
2020-03-12 23:54:39 +0900

    raphael: Optimize everything on preopt
    We have plenty of space left at /system
    
    Change-Id: I39d552259b66ec329c56cd4f4a7fc46fb2c07457
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- device.mk
+++ device.mk
@@ -63,6 +63,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     XiaomiParts
 
+# Dex
+PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
+
 # Display
 PRODUCT_PACKAGES += \
     libdisplayconfig \
