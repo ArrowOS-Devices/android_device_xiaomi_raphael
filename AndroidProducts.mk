975c76bfa97b2ba119c8bee0b00be791073feeaa
kubersharma001 <kubersharma001@gmail.com>
2020-08-11 21:44:18 +0530

    raphael: initial adaption to ArrowOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_raphael.mk
+    $(LOCAL_DIR)/arrow_raphael.mk
+
+COMMON_LUNCH_CHOICES := \
+    arrow_raphael-user \
+    arrow_raphael-userdebug \
+    arrow_raphael-eng
