259fd98652c3c8cef3e59d6e60343fbcff53fd01
kubersharma001 <kubersharma001@gmail.com>
2020-09-17 00:37:52 +0530

    raphael: Build libhwbinder and libhidltransport
    Change-Id: Ie811d4c1560fb4104674d1300319a2f6c3ee2985

--- device.mk
+++ device.mk
@@ -46,6 +46,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy_engine_configuration.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/audio_policy_engine_configuration.xml
 
+# Binder
+PRODUCT_PACKAGES += \
+    libhwbinder \
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     BluetoothQti
@@ -77,7 +81,8 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.manager@1.0
+    android.hidl.manager@1.0 \
+    libhidltransport \
 
 # HotwordEnrollement
 PRODUCT_COPY_FILES += \
