711d5b58fd8be9ee955cb3fcff2346aa39ab4fad
kubersharma001 <kubersharma001@gmail.com>
2020-10-09 23:08:01 +0530

    raphael: Adapt overlays
    Change-Id: I7c5e73a32f5cd2b942bcc9020f40b1fff3e62dc6

--- device.mk
+++ device.mk
@@ -21,7 +21,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-arrow
 
 # Properties
 -include $(LOCAL_PATH)/system_prop.mk
