db128ba758a5b01ea828d7cc417538d51595de39
kubersharma001 <kubersharma001@gmail.com>
2020-10-12 12:17:15 +0200

    raphael: Update makefile name for new QCOM sepolicy
    Change-Id: I96714846258d6c409ed9257da15ac2281bfd1d9b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -143,14 +143,9 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 
 # Sepolicy
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    $(DEVICE_PATH)/sepolicy/private \
-    device/qcom/sepolicy/generic/private \
-    device/qcom/sepolicy/qva/private
+include device/qcom/sepolicy/SEPolicy.mk
 
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    device/qcom/sepolicy/generic/public \
-    device/qcom/sepolicy/qva/public
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 
 # Power
 TARGET_USES_INTERACTION_BOOST := true
