0bc4c0b94a21cf25b08b676b093a16dda9a13d55
kubersharma001 <kubersharma001@gmail.com>
2020-12-07 12:20:29 +0000

    raphael: Set flag for Pixel charger animation.
    Change-Id: I7d6dd72876c63144e31da9f1117c27a9fa60cc1a

--- arrow_raphael.mk
+++ arrow_raphael.mk
@@ -20,3 +20,4 @@ DEVICE_MAINTAINER := kubersharma001
 BUILD_FINGERPRINT := "Xiaomi/raphaelin/raphaelin:9/PKQ1.181121.001/V10.3.3.0.PFKINXM:user/release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+TARGET_INCLUDE_PIXEL_CHARGER := true
