b6bed8fc5ccf41c178fae944581eacb285ed04b0
Kujou Yuko <i@779.moe>
2022-06-05 12:22:27 +0800

    raphael: Import Pixel thermal HAL
     * Checkout from android-12.1.0_r5.
    
    Signed-off-by: Kujou Yuko <i@779.moe>
    Change-Id: I346c5a9bddea8c3e82a364e92103662bad82ea59

--- device.mk
+++ device.mk
@@ -408,6 +408,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0-service.xiaomi \
+    thermal_symlinks
+
 # Touchscreen
 PRODUCT_PACKAGES += \
     libtinyxml2
